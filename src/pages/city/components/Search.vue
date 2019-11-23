9a3319a5f14b0702400f4812db16b12df33e1507
yingkun1 <925712087@qq.com>
2019-11-23 17:50:47 +0800

    23hao

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,65 @@
 <template>
-    <div class="search">
-        <input class="search-input" type="text" placeholder="输入城市名称或拼音"/>
+    <div>
+        <div class="search">
+            <input class="search-input" type="text" v-model="keyword"  placeholder="输入城市名称或拼音"/>
+        </div>
+        <div class="search-content" ref="search" v-show="keyword">
+            <ul>
+                <li  class="search-item border-bottom"  v-for="item in list" :key="item.id">{{item.name}}</li>
+                <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
+            </ul>
+        </div>
     </div>
+
+    
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-    name:'CitySearch'
+    name:'CitySearch',
+    data(){
+        return {
+            keyword:'',
+            list:[],
+            timer:null
+        }
+    },
+    computed:{
+        hasNoData(){
+            return !this.length
+        }
+    },
+    props:{
+        cities:Object
+    },
+    watch:{
+        keyword(){
+            // 节流
+            if(this.timer){
+                clearTimeout(this.timer)
+            }
+            if(!this.keyword){
+                this.list = []
+                return 
+            }
+            this.timer = setTimeout(()=>{
+                let result = []
+                for(let i in this.cities){
+                    this.cities[i].forEach(element => {
+                        if(element.spell.indexOf(this.keyword) > -1 || element.name.indexOf(this.keyword) > -1){
+                            result.push(element)
+                        }
+                    });
+                }
+                this.list = result
+            },100)
+
+        }
+    },
+    mounted(){
+        this.scroll = new Bscroll(this.$refs.search)
+    }
 }
 </script>
 
@@ -25,5 +78,19 @@ export default {
             border-radius .06rem
             color #666
             padding 0 .1rem
-           
+    .search-content
+        overflow hidden
+        z-index 1
+        background #eee
+        position absolute
+        top 1.58rem
+        left 0
+        right 0
+        bottom 0
+        .search-item
+            line-height .62rem
+            padding-left .2rem
+            color #666
+            background #fff
+            
 </style>
