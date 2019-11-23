af1d6f4a0a44a32f0c1ae0d56ff7416d14cc2d0f
yingkun1 <925712087@qq.com>
2019-11-23 15:09:48 +0800

    ajax city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,8 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
-        <city-list></city-list>
-        <city-alphabet></city-alphabet>
+        <city-list :cities="cities" :hotCities="hotCities"></city-list>
+        <city-alphabet :cities="cities"></city-alphabet>
     </div>
 </template>
 
@@ -12,6 +12,7 @@ import CityHeader from './components/Header'
 import CitySearch from './components/Search'
 import CityList from './components/List'
 import CityAlphabet from './components/Alphabet'
+import axios from 'axios'
 
 export default {
     name:'City',
@@ -20,6 +21,31 @@ export default {
         CitySearch,
         CityList,
         CityAlphabet
+    },
+    data(){
+        return {
+            cities:{},
+            hotCities:[]
+        }
+    },
+    methods:{
+        getCityInfo(){
+            axios.get('/static/mock/city.json')
+                .then(res => this.getCityInfoSync(res))
+        },
+        getCityInfoSync(res){
+            console.log(res)
+            res = res.data
+            if(res.ret && res.data){
+                this.cities = res.data.cities
+                this.hotCities = res.data.hotCities
+                console.log("cities:"+this.cities)
+                console.log("hotCities"+this.hotCities)
+            }
+        }
+    },
+    mounted(){
+        this.getCityInfo()
     }
 }
 </script>
--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,35 +1,15 @@
 <template>
     <ul class="list">
-        <li class="item">A</li>
-        <li class="item">B</li>
-        <li class="item">C</li>
-        <li class="item">D</li>
-        <li class="item">E</li>
-        <li class="item">F</li>
-        <li class="item">G</li>
-        <li class="item">H</li>
-        <li class="item">A</li>
-        <li class="item">B</li>
-        <li class="item">C</li>
-        <li class="item">D</li>
-        <li class="item">E</li>
-        <li class="item">F</li>
-        <li class="item">G</li>
-        <li class="item">H</li>
-        <li class="item">A</li>
-        <li class="item">B</li>
-        <li class="item">C</li>
-        <li class="item">D</li>
-        <li class="item">E</li>
-        <li class="item">F</li>
-        <li class="item">G</li>
-        <li class="item">H</li>
+        <li class="item" v-for="(item,key) of cities" :key="key">{{key}}</li>
     </ul>
 </template>
 
 <script>
 export default {
-    name:'CityAlphabet'
+    name:'CityAlphabet',
+    props:{
+        cities:Object
+    }
 }
 </script>
 
@@ -44,7 +24,6 @@ export default {
         right 0
         bottom 0
         width .4rem
-        background red
         .item
             text-align center
             line-height .4rem
