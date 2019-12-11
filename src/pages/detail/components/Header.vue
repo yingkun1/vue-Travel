6d0eff88c820d59cd7b02cb429afa037f553579c
yingkun1 <925712087@qq.com>
2019-12-11 14:46:30 +0800

    fix problem

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -30,6 +30,7 @@ export default {
     },
     methods:{
         handleScroll(){
+            console.log("scroll")
             const top = document.documentElement.scrollTop;
             if(top>50 && top<130){
                 let opacity = top/140
@@ -45,6 +46,9 @@ export default {
     },
     activated(){
         window.addEventListener('scroll',this.handleScroll)
+    },
+    deactivated(){
+        window.removeEventListener('scroll',this.handleScroll)
     }
 }
 </script>
