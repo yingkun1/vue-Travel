c7c314300e21e31745389878bb3a561afec422a5
yingkun1 <925712087@qq.com>
2019-12-11 13:56:13 +0800

    banner finish

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,21 +1,46 @@
 <template>
-    <div class="banner">
-        <img class="banner-img" src="https://imgs.qunarzz.com/p/tts5/1811/2a/f605ec46c493b302.jpg_r_390x260x90_ab06434f.jpg" alt="">
-        <div class="banner-info">
-            <div class="banner-title">大连圣亚海洋公园(AAAA景区)</div>
-            <div class="banner-number">
-                <span class="iconfont banner-icon">&#xe692;</span>
-                39
+    <div>
+        <div class="banner" @click="handleBannerClick">
+            <img class="banner-img" src="https://imgs.qunarzz.com/p/tts5/1811/2a/f605ec46c493b302.jpg_r_390x260x90_ab06434f.jpg" alt="">
+            <div class="banner-info">
+                <div class="banner-title">大连圣亚海洋公园(AAAA景区)</div>
+                <div class="banner-number">
+                    <span class="iconfont banner-icon">&#xe692;</span>
+                    39
+                </div>
             </div>
         </div>
-
+        <common-gallery :imgs="imgs" v-show="showGarrery" ></common-gallery>
     </div>
 
 </template>
 
 <script>
+import CommonGallery from 'common/gallery/Gallery'
 export default {
-
+    name:'DetailBanner',
+    components:{
+        CommonGallery
+    },
+    methods:{
+        handleBannerClick(){
+            this.showGarrery = true;
+        },
+    },
+    data(){
+        return {
+            showGarrery:false,
+            imgs:[
+                'https://imgs.qunarzz.com/p/tts9/1811/ae/e1616477abfd4702.jpg_r_390x260x90_e888ee6e.jpg',
+                'https://imgs.qunarzz.com/p/tts0/1811/a6/db6c599edf772602.jpg_r_390x260x90_867e69c3.jpg'
+            ]
+        }
+    },
+    methods:{
+        handleBannerClick(){
+            this.showGarrery = true
+        }
+    }
 }
 </script>
 
@@ -51,3 +76,4 @@ export default {
                 .banner-icon
                     font-size .24rem
 </style>
+  
